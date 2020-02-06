2b39bc6ef77f83e94a3189e79bda140733a9e4f4
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-02-06 13:57:37 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,19 +1,12 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class Kid 
+class Kid
   include Dance
-  
-   extend MetaDancing
-   
-  attr_accessor :name 
-  
+
+  attr_accessor :name
+
   def initialize(name)
-    
     @name = name
-    
   end
-  
-  
-  
 end
